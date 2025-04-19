2de6475dcf0a88ad599fff502da9541ffc6cf7fa
Taras Struk <t.struk@sonnen.de>
2025-04-19 19:22:08 +0200

    simple DShot protocol tester on RP2040

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# dshot-reader-test
+## DShot reader test
+
+This binary crate is a minitest for DShot protocol communication.
+
+For the moment it is tuned for "listening" to DShot 300.
+
+The received messages are decoded 
+and can be observed as human-readable logs through a serial terminal.
+
+### Hardware
+
+On the "publisher side" a sender of DShot data signal
+with level `3.3V` is required.
+
+On the "consumer side" one RP2040 and one UART USB adapter 
+are required to run the program and read the logs.
+
+### Connection
+
+- connect RP2040 GPIO 4 pin to RX pin on the UART adapter;
+- connect RP2040 signal ground to the ground of UART adapter;
+- connect RP2040 GPIO 0 to the source of DShot signals;
+- connect RP2040 signal ground to the ground of DSHot source;
+
+### Installation
+
+The compiled binary could be installed to PR2040 when it's
+connected to the host computer as USB mass storage.
+Then run `cargo r`.
+
+### Debugging
+
+The signals on GPIOs 0 and 1 could be compared on oscilloscope.
+
+Debug-signal on the pin GPIO 1 pulls shortly up and down
+when the logical "1" on the source sequence is detected.
---
+++ src/main.rs
@@ -0,0 +1,121 @@
+#![no_std]
+#![no_main]
+
+use core::fmt::Write;
+use hal::{
+    entry,
+    gpio::{self, FunctionPio0, Pin},
+    pio::{PIOBuilder, PIOExt, PinDir, ShiftDirection},
+    uart::{DataBits, StopBits, UartConfig},
+    {Sio, pac},
+};
+use panic_halt as _;
+use rp2040_hal as hal;
+use rp2040_hal::fugit::RateExtU32;
+use rp2040_hal::{Clock, clocks};
+
+#[unsafe(link_section = ".boot_loader")]
+#[used]
+pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+pub const XOSC_CRYSTAL_FREQ: u32 = 12_000_000;
+
+#[entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+
+    let sio = Sio::new(pac.SIO);
+    let pins = gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    // Configure the clocks
+    let clocks = clocks::init_clocks_and_plls(
+        XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+
+    let pin0: Pin<_, FunctionPio0, _> = pins.gpio0.into_function();
+    let pin1: Pin<_, FunctionPio0, _> = pins.gpio1.into_function();
+    let data_pin_id = pin0.id().num;
+    let debug_pin_id = pin1.id().num;
+
+    let program = pio_proc::pio_asm!(
+        ".wrap_target",
+        "set x, 1"
+        "begin:",
+        "  set pins, 0"
+        "  wait 1 pin 0",
+        "  nop [5]"
+        "  jmp pin one",
+        "zero:",
+        "  in null, 1",
+        "  jmp begin",
+        "one:",
+        "  in x, 1",
+        "  set pins, 1"
+        "  wait 0 pin 0",
+        ".wrap"
+    );
+
+    // Initialize and start PIO
+    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    let installed = pio.install(&program.program).unwrap();
+
+    // DShot 300 requires a double frequency of the producer.
+    // So the clock divisor of the producer is `(50, 0)` we need to use `(25, 0)` here.
+    //
+    let (int, frac) = (25, 0);
+    let (mut sm, mut rx, _) = PIOBuilder::from_installed_program(installed)
+        .set_pins(debug_pin_id, 1)
+        .jmp_pin(data_pin_id)
+        .autopush(true)
+        .push_threshold(16)
+        .clock_divisor_fixed_point(int, frac)
+        .in_shift_direction(ShiftDirection::Left)
+        .build(sm0);
+    sm.set_pindirs([(debug_pin_id, PinDir::Output)]);
+    sm.start();
+
+    let uart_pins = (
+        // UART TX on pin 4
+        pins.gpio4.into_function(),
+        // UART RX on pin 5
+        pins.gpio5.into_function(),
+    );
+    let mut uart = hal::uart::UartPeripheral::new(pac.UART1, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(115200.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
+
+    uart.write_full_blocking(b"UART is started\r\n");
+
+    loop {
+        if let Some(value) = rx.read() {
+            let crc = value & 0b1111;
+            let throttle = value >> 4;
+            let expected_crc = (throttle ^ (throttle >> 4) ^ (throttle >> 8)) & 0b1111;
+            let crc_ok = crc == expected_crc;
+            let telemetry = throttle & 1;
+            let throttle = throttle >> 1;
+            writeln!(uart, "value: {value:#b}\r\n").unwrap();
+            writeln!(
+                uart,
+                "throttle: {throttle}\r\ncrc: {crc}  ok: {crc_ok:?}\r\ntm: {telemetry}\r\n"
+            )
+            .unwrap();
+        }
+    }
+}
